65c3ceb3ce254d170944a6ff05c0242a08d1c821
Frcomparan <frcomparan@gmail.com>
2023-05-06 19:32:52 -0600

    Create model user with authentication

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: %i[ private ]
+
   def home; end
+
+  def private; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get '/private', to: 'pages#private', as: 'private'
   # Defines the root path route ("/")
   root 'pages#home'
 end
