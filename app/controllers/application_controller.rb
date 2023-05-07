6458103b8db57e8449ed55005fd9ea91de737234
Frcomparan <frcomparan@gmail.com>
2023-05-06 19:38:00 -0600

    Add translation for devise locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = 'es'
+  end
 
   protected
 
