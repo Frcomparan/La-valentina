8bf5d109b0c80a7a27ecc9dd97b1a9c7122e9a04
Frcomparan <frcomparan@gmail.com>
2023-05-11 07:26:57 -0600

    Add design for lesson comments

--- app/models/score.rb
+++ app/models/score.rb
@@ -3,6 +3,6 @@ class Score < ApplicationRecord
   belongs_to :course
 
   validates :value, presence: true
-  validates :value, numericality: { greater_than: 0, less_than: 5, message: 'La calificación debe estar entre 0 y 5' }
+  validates :value, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5, message: 'La calificación debe estar entre 0 y 5' }
 
 end
